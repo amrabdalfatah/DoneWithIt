3edeefa7435b52ed6e9debd92a09d5fd62ba357b
amrabdalfatah <amr.abdalfatah.mohamed@gmail.com>
2022-08-30 04:50:58 +0200

    Account Screen

--- App.js
+++ App.js
@@ -8,7 +8,8 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
+import AccountScreen from "./app/screens/AccountScreen";
 
 export default function App() {
-  return <ListingsScreen />;
+  return <AccountScreen />;
 }
--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -5,14 +5,15 @@ import colors from "../config/colors";
 
 import AppText from "./AppText";
 
-function ListItem({ image, title, subTitle, onPress }) {
+function ListItem({ image, title, subTitle, onPress, IconComponent }) {
   return (
     <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
       <View style={styles.container}>
-        <Image source={image} style={styles.image} />
-        <View>
+        {IconComponent}
+        {image && <Image source={image} style={styles.image} />}
+        <View style={styles.detailsContainer}>
           <AppText style={styles.title}>{title}</AppText>
-          <AppText style={styles.subTitle}>{subTitle}</AppText>
+          {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
         </View>
       </View>
     </TouchableHighlight>
@@ -23,6 +24,10 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
     padding: 15,
+  },
+  detailsContainer: {
+    marginLeft: 10,
+    justifyContent: "center",
   },
   image: {
     width: 70,
