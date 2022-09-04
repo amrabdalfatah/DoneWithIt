fa531aeebc7ef3dd1006b7bce44afcb4c3349d13
amrabdalfatah <amr.abdalfatah.mohamed@gmail.com>
2022-09-04 13:32:16 +0200

    Add Images to Listing

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
 import LoginScreen from "./app/screens/LoginScreen";
 import RegisterScreen from "./app/screens/RegisterScreen";
@@ -8,36 +8,7 @@ import ListingsScreen from "./app/screens/ListingsScreen";
 import MessageScreen from "./app/screens/MessagesScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
-import * as ImagePicker from "expo-image-picker";
-import Screen from "./app/components/Screen";
-import { Button, Image } from "react-native";
 
 export default function App() {
-  const [imageUri, setImageUri] = useState();
-
-  const requestPermission = async () => {
-    const result = await ImagePicker.requestCameraPermissionsAsync();
-    if (!result.granted)
-      alert("you need to enable permissions to access your photo");
-  };
-
-  useEffect(() => {
-    requestPermission();
-  }, []);
-
-  const selectImage = async () => {
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync();
-      if (!result.cancelled) setImageUri(result.uri);
-    } catch (error) {
-      console.log("Error when reading image", error);
-    }
-  };
-
-  return (
-    <Screen>
-      <Button title="Select Image" onPress={selectImage} />
-      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
-    </Screen>
-  );
+  return <ListingEditScreen />;
 }
